4ff2c7624b00961068da9150919966656a3cbe15
Bruna Oliveira <bruna.barbosa@ccc.ufcg.edu.br>
2014-06-20 14:00:03 -0300

    last

--- test/IntegrationTest.java
+++ test/IntegrationTest.java
@@ -15,6 +15,9 @@ public class IntegrationTest {
      * add your integration test here
      * in this example we just check if the welcome page is being shown
      */
-
+    @Test
+    public void test() {
+      
+    }
 
 }
